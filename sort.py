00809c346c35223c9b81b159cf168bd6015b918e
xiangyizan <xiangyizan@aliyun.com>
2020-05-05 22:35:26 +0800

    sort 123

--- sort.py
+++ sort.py
@@ -7,6 +7,31 @@ import time
 
 def parse_args():
     parser = argparse.ArgumentParser(description="SORT demo")
+    parser.add_argument('--display', dest='display', help='Display online tracker output (slow) [False]',
+                        action='store_true')
+    parser.add_argument("--seq_path", help="Path to detections.", type=str, default='data')
+    parser.add_argument("--phase", help="Subdirectory in seq_path.", type=str, default='train')
+    args = parser.parse_args()
+    return args
+
 
 if __name__ == '__main__':
-    print("hello world")
+    # print("hello world")
+    args = parse_args()
+    display = args.display
+    phase = args.phase
+    colours = np.random.rand(32, 3)
+    if (display):
+        if not os.path.exists('mot_benchmark'):
+            print(
+                '\n\tERROR: mot_benchmark link not found!\n\n    Create a symbolic link to the MOT benchmark\n    (https://motchallenge.net/data/2D_MOT_2015/#download). E.g.:\n\n    $ ln -s /path/to/MOT2015_challenge/2DMOT2015 mot_benchmark\n\n')
+            exit()
+        plt.ion()
+        fig = plt.figure()
+        ax1 = fig.add_subplot(111, aspect='equal')
+        print(os.path.exists('mot_benchmark'))
+    if not os.path.exists('output'):
+        os.makedirs('output')
+    # print(display)
+    # print(phase)
+    # print(args)
